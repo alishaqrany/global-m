b22d6e35b2951cbcf9bb197f0e56a9c7274a452a
ali shakrany <alishakrany@gmail.com>
2023-11-19 16:43:29 +0200

    add auto year update

--- includes/footer.php
+++ includes/footer.php
@@ -17,6 +17,35 @@
     <!-- Custom Theme JavaScript -->
     <script src="assets/js/sb-admin-2.js"></script>
     <script src="assets/js/jquery.validate.min.js"></script>
+    
+    
+    
+        <script>
+        function updateManufacturingYear() {
+            // الحصول على قيمة رقم شاسيه السيارة
+            var chassisNumber = document.getElementById('chassisNo').value;
+
+            // الحصول على الحرف العاشر من جهة اليسار
+            var character = chassisNumber.charAt(9).toUpperCase(); // تحويل الحرف إلى صيغة كبيرة
+
+            // جدول التحويل
+            var yearTable = {
+                'Y': 2000, '1': 2001, '2': 2002, '3': 2003, '4': 2004, '5': 2005,
+                '6': 2006, '7': 2007, '8': 2008, '9': 2009, 'A': 2010, 'B': 2011,
+                'C': 2012, 'D': 2013, 'E': 2014, 'F': 2015, 'G': 2016, 'H': 2017,
+                'J': 2018, 'K': 2019, 'L': 2020, 'M': 2021, 'N': 2022, 'P': 2023,
+                'R': 2024, 'S': 2025, 'T': 2026, 'V': 2027, 'W': 2028, 'X': 2029
+            };
+
+            // تحديد سنة التصنيع
+            var manufacturingYear = yearTable[character];
+            
+            // عرض سنة التصنيع في الحقل المخصص
+            document.getElementById('year').value = manufacturingYear;
+        }
+    </script>
+    
+    
 
 </body>
 
--- personal.php
+++ personal.php
@@ -124,7 +124,7 @@ include BASE_PATH . '/includes/header.php';
 </div>
 <div class="form-group">
   <label for="chassisNo">Chassis No</label>
-  <input type="text" class="form-control" id="chassisNo" name="chassisNo" required>
+  <input type="text" class="form-control" id="chassisNo" name="chassisNo" oninput="updateManufacturingYear()" required>
 </div>
 <div class="form-group">
   <label for="quantity">Quantity</label>
@@ -231,6 +231,46 @@ include BASE_PATH . '/includes/header.php';
 <button type="submit" class="btn btn-primary">Submit</button>
 </form>
 </div>
+
+
+
+
+
+
+
+
+
+
+
+    <script>
+        function updateManufacturingYear() {
+            // الحصول على قيمة رقم شاسيه السيارة
+            var chassisNumber = document.getElementById('chassisNo').value;
+
+            // الحصول على الحرف العاشر من جهة اليسار
+            var character = chassisNumber.charAt(9).toUpperCase(); // تحويل الحرف إلى صيغة كبيرة
+
+            // جدول التحويل
+            var yearTable = {
+                'Y': 2000, '1': 2001, '2': 2002, '3': 2003, '4': 2004, '5': 2005,
+                '6': 2006, '7': 2007, '8': 2008, '9': 2009, 'A': 2010, 'B': 2011,
+                'C': 2012, 'D': 2013, 'E': 2014, 'F': 2015, 'G': 2016, 'H': 2017,
+                'J': 2018, 'K': 2019, 'L': 2020, 'M': 2021, 'N': 2022, 'P': 2023,
+                'R': 2024, 'S': 2025, 'T': 2026, 'V': 2027, 'W': 2028, 'X': 2029
+            };
+
+            // تحديد سنة التصنيع
+            var manufacturingYear = yearTable[character];
+            
+            // عرض سنة التصنيع في الحقل المخصص
+            document.getElementById('year').value = manufacturingYear;
+        }
+    </script>
+
+
+
+
+
 
 
 </div>
